506aac670132d283ef8be5172365a29efd54af6d
benjyGarrison <benjygarrison@gmail.com>
2019-09-13 01:03:47 -0700

    Initial Commit

---
+++ Salarize/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Salarize
+//
+//  Created by Benjamin Garrison on 9/13/19.
+//  Copyright © 2019 Benji Garrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
